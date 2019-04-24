995a45d74a5b7263ecbaf9e874f2d73d6cf95a7a
polodev <polodev10@gmail.com>
2019-04-24 17:46:58 +0600

    updated seeder

--- index.php
+++ index.php
@@ -13,8 +13,9 @@ $comment_generator = new CommentGenerator();
 
 Comment::truncate();
 foreach ($posts as $post) {
+	shuffle($comments);
 	foreach ($comments as $comment) {
-		$probality = [true, false, true];
+		$probality = [true, false, true, false, true, true];
 		$bool = $probality[array_rand($probality)];
 		if ($bool) {
 			$comment_generator->add_comment($comment, $post->ID );
